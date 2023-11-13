34ef65d2c2f9eac6b7213afc420c6b7a7811425b
eggshell <edzeljohnsinfuego@gmail.com>
2023-11-13 14:01:37 +0800

    fix comment

--- post.php
+++ post.php
@@ -11,6 +11,58 @@ if(isset($_GET['id'])) {
     die();
 }
 
+function time_ago($timestamp) {
+    $time_ago = strtotime($timestamp);
+    $current_time = time();
+    $time_difference = $current_time - $time_ago;
+    $seconds = $time_difference;
+    $minutes = round($seconds / 60);
+    $hours = round($seconds / 3600);
+    $days = round($seconds / 86400);
+    $weeks = round($seconds / 604800);
+    $months = round($seconds / 2629440);
+    $years = round($seconds / 31553280);
+
+    if ($seconds <= 60) {
+        return "just now";
+    } else if ($minutes <= 60) {
+        if ($minutes == 1) {
+            return "1 minute ago";
+        } else {
+            return "$minutes minutes ago";
+        }
+    } else if ($hours <= 24) {
+        if ($hours == 1) {
+            return "1 hour ago";
+        } else {
+            return "$hours hours ago";
+        }
+    } else if ($days <= 7) {
+        if ($days == 1) {
+            return "yesterday";
+        } else {
+            return "$days days ago";
+        }
+    } else if ($weeks <= 4.3) {
+        if ($weeks == 1) {
+            return "1 week ago";
+        } else {
+            return "$weeks weeks ago";
+        }
+    } else if ($months <= 12) {
+        if ($months == 1) {
+            return "1 month ago";
+        } else {
+            return "$months months ago";
+        }
+    } else {
+        if ($years == 1) {
+            return "1 year ago";
+        } else {
+            return "$years years ago";
+        }
+    }
+}
 ?>
 
 
@@ -83,21 +135,12 @@ if(isset($_GET['id'])) {
 								$comment_by = $comment_row['firstname']." ".  $comment_row['lastname'];
 							?>
 					<br><a href="#"><?php echo $comment_by; ?></a> - <?php echo $comment_row['comment']; ?>
-					<br>
+					<br><time>
 							<?php				
-								$days = floor($comment_row['TimeSpent'] / (60 * 60 * 24));
-								$remainder = $comment_row['TimeSpent'] % (60 * 60 * 24);
-								$hours = floor($remainder / (60 * 60));
-								$remainder = $remainder % (60 * 60);
-								$minutes = floor($remainder / 60);
-								$seconds = $remainder % 60;
-								if($days > 0)
-								echo date('F d, Y - H:i:sa', $comment_row['date_posted']);
-								elseif($days == 0 && $hours == 0 && $minutes == 0)
-								echo "A few seconds ago";		
-								elseif($days == 0 && $hours == 0)
-								echo $minutes.' minutes ago';
+								
+                                echo time_ago($comment_row['date_posted']);
 							?>
+                            </time>
 					<br>
 							<?php
 							}
